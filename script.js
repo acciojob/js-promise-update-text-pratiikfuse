cf4420e3aa815a8b87c6b96a163452cd6161ff22
Pratik Fuse <pratikfuse20@gmail.com>
2023-10-21 21:07:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+async function fun(){
+	return setTimeOut(()=>"Hello, World!",1000);
+}
+fun().then((res)=>{
+	document.getElementById("output").innerText  = res;
+})
